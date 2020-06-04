ec05c5a51f57d743d550129c863941b0e7cf379d
Renan Aragão <refaragao@gmail.com>
2020-06-03 23:29:25 -0300

    plane geometry

--- main.js
+++ main.js
@@ -30,9 +30,15 @@ function init() {
     var material = new THREE.LineBasicMaterial({ color: 0x0000ff });
 
     var points = [];
-    points.push(new THREE.Vector3(-300, -100, 100));
+    points.push(new THREE.Vector3(-300, 0, 100));
     points.push(new THREE.Vector3(-300, 100, 100));
-    // points.push(new THREE.Vector3(200, 200, 0));
+    points.push(new THREE.Vector3(-180, 100, 100));
+    // points.push(new THREE.Vector3(-180, 0, 100));
+    // points.push(new THREE.Vector3(-300, 0, 100));
+
+
+
+
     // points.push(new THREE.Vector3(200, 0, 0));
     // points.push(new THREE.Vector3(0, 0, 0));
     // points.push(new THREE.Vector3(0, 0, -600));
@@ -55,18 +61,25 @@ function init() {
     raycaster = new THREE.Raycaster();
 
     var line = new THREE.Line(square, material);
+    line.name = 'quadrante-01';
     scene.add(line);
+
+    var planeGeometry = new THREE.PlaneGeometry(120, 120).setFromPoints();
+    var planeMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00, side: THREE.DoubleSide });
+    var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    plane.name = 'plane-01'
+    scene.add(plane);
 
     mesh = new THREE.Mesh(geometry, new THREE.MeshPhongMaterial({
         color: 0xFFFFFF,
         opacity: 0.5,
         transparent: true,
-      }));
+    }));
     scene.add(mesh);
 
     const gridHelper = new THREE.GridHelper(1000, 20);
     scene.add(gridHelper);
-    
+
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.render(scene, camera);
     renderer.setPixelRatio(window.devicePixelRatio);
@@ -98,6 +111,9 @@ function onDocumentMouseDown(event) {
     raycaster.set(camera.position, vector.sub(camera.position).normalize());
 
     var intersects = raycaster.intersectObjects(scene.children);
+
+    console.log(intersects);
+
     if (intersects.length > 0) {
         var index = Math.floor(intersects[0].faceIndex / 2);
         switch (index) {
@@ -142,66 +158,3 @@ function animate() {
     renderer.render(scene, camera);
 
 }
-
-function GridBoxGeometry(geometry, independent) {
-    // @author prisoner849
-    if (!(geometry instanceof THREE.BoxBufferGeometry)) {
-        console.log("GridBoxGeometry: the parameter 'geometry' has to be of the type THREE.BoxBufferGeometry");
-        return geometry;
-    }
-    independent = independent !== undefined ? independent : false;
-
-    let newGeometry = new THREE.BoxBufferGeometry();
-    let position = geometry.attributes.position;
-    newGeometry.attributes.position = independent === false ? position : position.clone();
-
-    let segmentsX = geometry.parameters.widthSegments || 1;
-    let segmentsY = geometry.parameters.heightSegments || 1;
-    let segmentsZ = geometry.parameters.depthSegments || 1;
-
-    let startIndex = 0;
-    let indexSide1 = indexSide(segmentsZ, segmentsY, startIndex);
-    startIndex += (segmentsZ + 1) * (segmentsY + 1);
-    let indexSide2 = indexSide(segmentsZ, segmentsY, startIndex);
-    startIndex += (segmentsZ + 1) * (segmentsY + 1);
-    let indexSide3 = indexSide(segmentsX, segmentsZ, startIndex);
-    startIndex += (segmentsX + 1) * (segmentsZ + 1);
-    let indexSide4 = indexSide(segmentsX, segmentsZ, startIndex);
-    startIndex += (segmentsX + 1) * (segmentsZ + 1);
-    let indexSide5 = indexSide(segmentsX, segmentsY, startIndex);
-    startIndex += (segmentsX + 1) * (segmentsY + 1);
-    let indexSide6 = indexSide(segmentsX, segmentsY, startIndex);
-
-    let fullIndices = [];
-    fullIndices = fullIndices.concat(indexSide1);
-    fullIndices = fullIndices.concat(indexSide2);
-    fullIndices = fullIndices.concat(indexSide3);
-    fullIndices = fullIndices.concat(indexSide4);
-    fullIndices = fullIndices.concat(indexSide5);
-    fullIndices = fullIndices.concat(indexSide6);
-
-    newGeometry.setIndex(fullIndices);
-
-    function indexSide(x, y, shift) {
-        let indices = [];
-        for (let i = 0; i < y + 1; i++) {
-            let index11 = 0;
-            let index12 = 0;
-            for (let j = 0; j < x; j++) {
-                index11 = (x + 1) * i + j;
-                index12 = index11 + 1;
-                let index21 = index11;
-                let index22 = index11 + (x + 1);
-                indices.push(shift + index11, shift + index12);
-                if (index22 < ((x + 1) * (y + 1) - 1)) {
-                    indices.push(shift + index21, shift + index22);
-                }
-            }
-            if ((index12 + x + 1) <= ((x + 1) * (y + 1) - 1)) {
-                indices.push(shift + index12, shift + index12 + x + 1);
-            }
-        }
-        return indices;
-    }
-    return newGeometry;
-};
